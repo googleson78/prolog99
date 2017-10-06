87181c6e9ed30ede3af022ad4e2c9a21fa1dce2a
Georgi Lyubenov <godzbanebane@gmail.com>
2017-10-06 22:37:43 +0300

    unsuccessful attempt at making it work as I intend

--- kth_elem.pl
+++ kth_elem.pl
@@ -3,12 +3,21 @@
 %X = c
 
 take(0, _, []).
-take(K, [X|Xs], [X|Ys]):- number(K), K1 is K - 1, take(K1, Xs, Ys).
+take(K, [X|Xs], [X|Ys]):- number(K), succ(K1, K), take(K1, Xs, Ys).
+
+
+minus_one(X, Y):- succ(Y, X).
+
+equal(0, 0).
+equal(X, Y):- succ(X1, X), succ(Y1, Y), equal(X1, Y1).
+
+take2(0, _, []).
+take2(K, [X|Xs], [X|Ys]):- number(K), succ(K1, K), take2(K1, Xs, Ys).
 
 drop(0, Xs, Xs).
 drop(K, Xs, Ys):- number(K), take(K, Xs, H), append(H, Ys, Xs).
 
-element_at(X, Ys, K):- number(K), K1 is K - 1, drop(K1, Ys, [X|_]).
+element_at(X, Ys, K):- number(K), succ(K1, K), drop(K1, Ys, [X|_]).
 
 %TODO: make drop/take/element_at work like this:
 %?- take(X, [a,b,c], [a,b]).
