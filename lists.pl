57da9e0c12a3a2213d54815fc393fa62fa0e7286
Georgi Lyubenov <godzbanebane@gmail.com>
2017-11-14 17:02:39 +0200

    added some sublist predicates

--- lists.pl
+++ lists.pl
@@ -5,5 +5,12 @@ prefix([X|Xs], [X|Ys]):- prefix(Xs, Ys).
 
 suffix(Xs, Ys):- prefix(Xs, PR), append(PR, Ys, Xs).
 
-sublists(_, []).
-sublists(Xs, Ys):- prefix(Xs, PR), suffix(PR, Ys), not(empty(Ys)).
+sublist([], _).
+sublist(X, Y):- prefix(Y, PR), suffix(PR, X), not(empty(X)).
+
+%only_subl(X,Y) is true if every element of x is a sublist of Y
+only_subl(X, Y):- not((member(Z, X), not(sublist(Z, Y)))).
+
+%combining with a "subset" predicate we now get 
+%X contains all (and only) sublists of Y
+all_subl(X, Y):- only_subl(X, Y), not((sublist(Z, Y), not(member(Z, X)))).
